b74c8cbafcaf6b37db68b871b4da659938ac10ce
503-08 <a503-08@503-08ui-iMac.local>
2018-11-19 10:17:39 +0900

    Initial Commit

---
+++ WebDataParsing1119/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebDataParsing1119
+//
+//  Created by 503-08 on 19/11/2018.
+//  Copyright © 2018 leewoomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
